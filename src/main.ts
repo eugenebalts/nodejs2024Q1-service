5b20bd4c54597006faa04579d1b0662301395ac8
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-03-12 00:03:19 +0100

    chore: setup and configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Home Library Service')
+    .setDescription(
+      'Users can create, read, update, delete data about Artists, Tracks and Albums, add them to Favorites in their own Home Library!',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
